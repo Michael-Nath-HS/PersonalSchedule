22142987b7e5f46aecb955249aacc7c070a30184
Michael <mnath10@stuy.edu>
2019-08-10 17:36:39 -0400

    "Added a way to know the previous and upcoming events"

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ window.onload = function() {
         "SAT Studying (ugh)": [21, 31, 22, 0],
         "Shower Time!": [22, 0, 22, 30],
         "Eat and Relax": [22, 31, 23, 59]
-
     };
+    var Keys = Object.keys(things);
+
     for (var key in things) {
         var allThings = document.createElement("h4");
         var toDo = document.createTextNode((key + ": " + things[key][0] + ":" +
@@ -36,9 +37,25 @@ window.onload = function() {
         if (((today.getHours() * 60) + today.getMinutes()) > (things[key][0] * 60) + things[key][1] &&
             ((today.getHours() * 60) + today.getMinutes() < ((things[key][2] * 60) + things[key][3]))) {
             var picker = document.getElementById(key);
-            var highlighter = document.createAttribute("class")
+            var highlighter = document.createAttribute("class");
             highlighter.value = "text-danger";
             picker.setAttributeNode(highlighter);
+            document.getElementById("doing").innerHTML += key;
+            var loc = Keys.indexOf(key);
+            console.log(Keys[Keys.length]);
+            if (loc <= 0) {
+                document.getElementById("passed").innerHTML += Keys[Keys.length - 1];
+            }
+            else {
+                document.getElementById("passed").innerHTML += Keys[loc - 1];
+            }
+
+            if (loc === Keys.length - 2) {
+                document.getElementById("passed").innerHTML += Keys[0];
+            }
+            else {
+                document.getElementById("upcoming").innerHTML += Keys[loc + 1];
+            }
         }
     }
 };
