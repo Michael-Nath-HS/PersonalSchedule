9ff1d39848ef183e65b2f1da7e276a8b9a110ae7
Michael <mnath10@stuy.edu>
2019-08-10 16:49:55 -0400

    "Created external JS file that houses the code necessary to automatically update the color of a particular event. Update happens if current time is in the range of the event.

---
+++ script.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+    var today = new Date();
+    time = today.getHours() + ":" + (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
+    console.log(time);
+    document.getElementById("timeDisplay").innerHTML += time;
+
+
+    var things = {
+        "Sleep": [1, 0, 9, 0],
+        "Lab": [10, 0, 15, 30],
+        "Schnatter's Lab": [16, 0, 18, 30],
+        "Chemistry Olympiad Study Time!": [19, 30, 21, 30],
+        "SAT Studying (ugh)": [21, 31, 22, 0],
+        "Shower Time!": [22, 0, 22, 30],
+        "Eat and Relax": [22, 31, 23, 59]
+
+    };
+    for (var key in things) {
+        var allThings = document.createElement("h4");
+        var toDo = document.createTextNode((key + ": " + things[key][0] + ":" +
+            (things[key][1] < 10 ? '0' : '') +
+            things[key][1] + "-" + things[key][2] + ":" +
+            (things[key][3] < 10 ? "0" : '') + things[key][3]));
+        var thingContainer = document.getElementById("things");
+        allThings.appendChild(toDo);
+        thingContainer.appendChild(allThings);
+        var allColor = document.createAttribute("class");
+        var discern = document.createAttribute("id");
+        allColor.value = "text-success";
+        discern.value = key;
+        allThings.setAttributeNode(allColor);
+        allThings.setAttributeNode(discern);
+    }
+
+    for (var key in things) {
+        if (((today.getHours() * 60) + today.getMinutes()) > (things[key][0] * 60) + things[key][1] &&
+            ((today.getHours() * 60) + today.getMinutes() < ((things[key][2] * 60) + things[key][3]))) {
+            var picker = document.getElementById(key);
+            var highlighter = document.createAttribute("class")
+            highlighter.value = "text-danger";
+            picker.setAttributeNode(highlighter);
+        }
+    }
+};
